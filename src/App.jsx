613da50d29b350dd6874506eb25c20dfefecb80a
Kshitij Jain <2024btechaimlkshitij18489@poornima.edu.in>
2026-03-30 14:09:16 +0530

    Update the landina dn login

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Home from './pages/Home';
 import Landing from './pages/Landing';
+import Login   from './pages/Login';
+import Home    from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/"       element={<Landing />} />
-        <Route path="/login"  element={<Login />} />
-        <Route path="/home"   element={<Home />} />
+        <Route path="/"      element={<Landing />} />
+        <Route path="/login" element={<Login />}   />
+        <Route path="/home"  element={<Home />}    />
       </Routes>
     </Router>
   );
